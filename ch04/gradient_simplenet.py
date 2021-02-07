dc0c818fa44eb457d14a700e1f7722779529857b
rjsgk789 <48079600+rjsgk789@users.noreply.github.com>
2021-02-07 15:28:54 +0900

    Update gradient_simplenet.py

--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -1,6 +1,10 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+#sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+
+#sys.path.append("C:\\Users\\user\\Documents\\GitHub\\deep-learning-from-scratch")
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import numpy as np
 from common.functions import softmax, cross_entropy_error
 from common.gradient import numerical_gradient
@@ -29,3 +33,5 @@ f = lambda w: net.loss(x, t)
 dW = numerical_gradient(f, net.W)
 
 print(dW)
+
+#print(sys.path)
